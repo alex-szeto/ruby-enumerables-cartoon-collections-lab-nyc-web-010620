f5da44b08719f97587fc40bf5e69bb201f9ad484
Alexander Szeto <alex.szeto.a@gmail.com>
2020-01-03 04:11:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 
 def summon_captain_planet(planet)
   # Your code here
-  output_array = planet.collect{|element| element[0].uppercase + "!"}
+  output_array = planet.collect{|element| element[0].upcase + "!"}
   puts output_array
   return output_array
 end
