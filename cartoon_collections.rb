4bb78a2bb5d59b748b579a0994c0a3365da03966
Alexander Szeto <alex.szeto.a@gmail.com>
2020-01-03 04:16:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,8 +21,8 @@ def long_planeteer_calls(calls)
   return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(list)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  if list in 
 end
